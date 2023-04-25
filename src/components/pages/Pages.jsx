e93e1ba72f1a6243db3a4f73185cdd0c19511b7f
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2023-04-25 18:45:49 +0700

    add pages content

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 
 export const Pages = () => {
+  const data = [
+    {
+      id: "01",
+      link: "https://cv-react.onrender.com",
+      linkName: "CV - Portfolio ",
+    },
+    {
+      id: "02",
+      link: "https://carttechnology.000webhostapp.com",
+      linkName: "Shopping Cart And Comment",
+    },
+    {
+      id: "03",
+      link: "https://news-api-95dr.onrender.com",
+      linkName: "News API",
+    },
+  ]
   return (
+
     <>
-    <section className="page">
-        <h1>Pages</h1>
-    </section>
+      <section className='pages'>
+
+        {data.map((value) => {
+          return (<div className='card '>
+
+            <h1>{value.linkName}</h1>
+            <a className='link' href='{value.link}' >{value.link} </a>
+
+
+          </div>)
+
+        }
+
+        )}
+
+      </section>
     </>
   )
 }
