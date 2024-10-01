3e8c05dd41d513437a7dfcac6119f6df93d25332
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2024-10-01 18:14:53 +0700

    add video

--- src/components/home/header/homes/Branding.jsx
+++ src/components/home/header/homes/Branding.jsx
@@ -1,47 +1,52 @@
 import React from 'react'
 import logo from '../../../../assets/Luen_logo.png'
+import video from '../../../../assets/Luen-Intro.mp4'
 
 export const Branding = () => {
-    const data = [
-        {
-          id: "01",
-          heading: "Digital Branding",
-          desc: "I always want to create quality products.",
-        },
-        {
-          id: "02",
-          heading: "Team Management",
-          desc: "Working with team is also an important thing in programming. ",
-        },
-        {
-          id: "03",
-          heading: "Creative Mind",
-          desc: "Creativity will be thr core to develop program.",
-        },
-      ]
+  const data = [
+    {
+      id: "01",
+      heading: "Digital Branding",
+      desc: "I always want to create quality products.",
+    },
+    {
+      id: "02",
+      heading: "Team Management",
+      desc: "Working with team is also an important thing in programming. ",
+    },
+    {
+      id: "03",
+      heading: "Creative Mind",
+      desc: "Creativity will be thr core to develop program.",
+    },
+  ]
   return (
     <>
-    <section  className='branding'>
+      <section className='branding'>
         <div className='container grid'>
-            {data.map((value) => {
-                return <div className='box flex'>
-                    <div className="text">
-                        <h1>{value.id}</h1>
-                    </div>
-                    <div className="para">
-                        <h2>{value.heading}</h2>
-                        <p>{value.desc}</p>
-                    </div>
-                </div>
+          {data.map((value) => {
+            return <div className='box flex'>
+              <div className="text">
+                <h1>{value.id}</h1>
+              </div>
+              <div className="para">
+                <h2>{value.heading}</h2>
+                <p>{value.desc}</p>
+              </div>
+            </div>
 
-            }
+          }
 
-            )}
+          )}
         </div>
-    </section>
-    <img width={'100%'} src={logo}/>
-    <iframe src="https://luen2003.github.io/Rubik.html"></iframe>
-    <iframe src="https://vietnam-map-platform.vercel.app/"></iframe>
+      </section>
+      <img width={'100%'} src={logo} />
+      <iframe src="https://luen2003.github.io/Rubik.html"></iframe>
+      <iframe src="https://vietnam-map-platform.vercel.app/"></iframe>
+      <video width="100%" controls autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </>
   )
 }
