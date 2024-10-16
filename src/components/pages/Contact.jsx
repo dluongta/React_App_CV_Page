3efae281721cd5404f27ec956e30633d578e9414
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2024-10-16 16:38:43 +0700

    add content

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,33 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Contact = () => {
     return (
         <>
             <section className="contact">
-                <div id="mySidenav" class="sidenav">
+                <div id="mySidenav" className="sidenav">
                     <Link to='/pages' id="pages"> Pages </Link>
                     <Link to='/blog' id="blog"> Blog </Link>
                     <Link to='/portfolio' id="portfolio"> Portfolio </Link>
                     <Link to='/contact' id="contact"> Contact </Link>
                 </div>
-                <div className="box">
-                    <div className="icon">
-                        <i className='fa fa-map-marker-alt' ></i>
-                        <label htmlFor=''>Location: Hanoi, Vietnam</label>
-                    </div>
-                    <div className="icon">
-                        <i className='fa fa-phone'></i>
-                        <label htmlFor=''>Phone: +123456789</label>
-                    </div>
-                    <div className="icon">
-                        <i className='fa fa-envelope'></i>
-                        <label htmlFor=''>Email: luen2k3@gmail.com</label>
-                    </div>
+                <div className="pdf-container">
+                    <iframe className="pdf" src="/TopCV.pdf" width="100%" height="500" title="TopCV"></iframe>
+                    <iframe className="pdf" src="/CV-TopCV.pdf" width="100%" height="500" title="CV-TopCV"></iframe>
+                    <iframe className="pdf" src="/CV-Joboko.pdf" width="100%" height="500" title="Joboko"></iframe>
                 </div>
-
             </section>
         </>
-    )
+    );
 }
